35b618e7bad99888690cee8251fe7b6df5e8dc11
Damian Krzeminski <krzemdamian@gmail.com>
2019-09-27 21:21:45 +0200

    routing to content outlet

--- cv-page-app/src/app/app-routing.module.ts
+++ cv-page-app/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'home',
+  loadChildren: './modules/home/home.module#HomeModule'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
