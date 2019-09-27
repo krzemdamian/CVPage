3d8d7a82aac876307e4a3542ead7b6d32c478244
Damian Krzeminski <krzemdamian@gmail.com>
2019-09-27 20:18:19 +0200

    Prepare MVP header

--- cv-page-app/src/app/app.module.ts
+++ cv-page-app/src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ cv-page-app/src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+  ],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent,
+  ]
+})
+export class CoreModule { }
