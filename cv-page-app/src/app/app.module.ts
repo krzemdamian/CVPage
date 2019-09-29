ce05a715464827c0cc86ab0d4e9f0104e78046d0
Damian Krzeminski <krzemdamian@gmail.com>
2019-09-29 13:16:08 +0200

    Clean up

--- cv-page-app/src/app/app.module.ts
+++ cv-page-app/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
