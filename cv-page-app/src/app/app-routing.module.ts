5a7dc3b0eba6a7bca2a651d75edc5647532abe68
Damian Krzeminski <krzemdamian@gmail.com>
2019-09-29 20:27:05 +0200

    Change routing

--- cv-page-app/src/app/app-routing.module.ts
+++ cv-page-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  { path: 'about', loadChildren: './modules/about/about.module#AboutModule' },
   { path: 'skills', loadChildren: './modules/skills/skills.module#SkillsModule' },
   { path: 'experiance', loadChildren: './modules/experiance/experiance.module#ExperianceModule' },
   { path: 'education', loadChildren: './modules/education/education.module#EducationModule' },
   { path: 'projects', loadChildren: './modules/projects/projects.module#ProjectsModule' },
-  { path: 'about', loadChildren: './modules/about/about.module#AboutModule' },
   { path: 'contact', loadChildren: './modules/contact/contact.module#ContactModule' },
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: '*', redirectTo: 'about', pathMatch: 'full' },
 ];
 
 @NgModule({
